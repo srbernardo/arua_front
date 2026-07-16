675c2660b35630c1ce36418610c5017e0cc49967
Bernardo Brotelli <b.brotelle@gmail.com>
2026-07-16 19:42:20 +0100

    Wrap App component with ProductsProvider for context management

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CartProvider } from './context/CartContext'
+import { ProductsProvider } from './context/ProductsContext'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <ProductsProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductsProvider>
   </StrictMode>,
 )
